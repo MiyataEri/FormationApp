449c4072bc3a1ae1f00a8c59f4c510b86d5d0bf8
宮田恵里 <miyaeri@nain.jp>
2017-10-04 11:15:29 +0900

    Initial Commit

---
+++ MiyataSample2Tests/MiyataSample2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MiyataSample2Tests.swift
+//  MiyataSample2Tests
+//
+//  Created by 宮田恵里 on 2017/10/04.
+//  Copyright © 2017年 宮田恵里. All rights reserved.
+//
+
+import XCTest
+@testable import MiyataSample2
+
+class MiyataSample2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
